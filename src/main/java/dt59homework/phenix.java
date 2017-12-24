19ccffaa3aaf0adcc0a6b185277a05525d1d5a59
lenovo <lenovo@idea-PC>
2017-12-24 15:33:36 +0800

    作业

---
+++ src/main/java/dt59homework/phenix.java
@@ -0,0 +1,26 @@
+package dt59homework;
+
+public class phenix {
+    String name;
+
+    String born;
+
+    String ability;
+
+    String legend;
+
+    int bodily_form;
+
+    void fly() {
+        System.out.println("xiu.xiu.xiu");
+    }
+
+    void jump() {
+        System.out.println("YOU jump, I  jump");
+    }
+
+    int niupi() {
+        return 666;
+    }
+}
+// 类名驼峰式，要求开头字母大写，写掉了，张琦老师不在，我待会再去找老师，备注还没加。
--- src/main/java/dt59homework/phoenix.java
+++ src/main/java/dt59homework/phoenix.java
@@ -3,8 +3,8 @@ package dt59homework;
 public class phoenix {
 
     public static void main(String[] args) {
-        Phenix phoenix;
-        phoenix = new Phenix();
+        phenix phoenix;
+        phoenix = new phenix();
         phoenix.name = "炎鸢";
         phoenix.born = "公元前84年";
         phoenix.ability = "涅槃重生";
@@ -16,4 +16,6 @@ public class phoenix {
         phoenix.fly();
         System.out.println(phoenix.niupi());
     }
+
 }
+// 类名驼峰式，要求开头字母大写，写掉了，张琦老师不在，我待会再去找老师，备注还没加。
